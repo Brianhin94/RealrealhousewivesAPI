cb5ce7749511d71d7c4cb66c40b960b280bd6ef9
ihjacobs925 <ihjacobs925@gmail.com>
2021-02-24 11:14:35 -0600

    end points

--- controllers/housewife.js
+++ controllers/housewife.js
@@ -12,16 +12,39 @@ router.get('/housewife', (req, res) => {
         res.json({ housewives })
         // .then((response) => {
         //     res.json(response)
-    })
+    });
 });
 
-// Detail/Show Route - GET /users/:id
+//Detail/Show Route - GET /users/:id
 router.get('/housewife/:id', (req, res) => {
     console.log(req.params);
-    db.findById(req.params.id, (err, db) => {
+    db.Housewife.findById(req.params.id, (err, db) => {
         if (err) {
             console.error(`🔥 Error in Housewife:id route:\n${err}`);
             return res.status(500).json({ error: 'Error in housewife:id route' });
+        }
+        res.json({ db });
+    });
+});
+
+//http://localhost:3002/api/searchname?first_name=Charrisse
+router.get('/SearchName', (req, res) => {
+    db.Housewife.findOne({first_name: req.query.first_name}, (err, db) => {
+        if (err) {
+            console.error(`Error in searchCity route:\n${err}`);
+            return res.status(500).json({ erro: `Error in searchCity route:\n${err}`});
+        }
+        res.json({ db });
+    });
+});
+    
+
+// http://localhost:3002/api/searchCity?city=Potomac
+router.get('/searchCity', (req, res) => {
+    db.Housewife.find({city: req.query.city}, (err, db) => {
+        if (err) {
+            console.error(`Error in searchCity route:\n${err}`);
+            return res.status(500).json({ erro: `Error in searchCity route:\n${err}`});
         }
         res.json({ db });
     });
